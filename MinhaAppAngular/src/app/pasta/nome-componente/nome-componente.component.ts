0865e49f18f6906d8ebe6c09303923418cec1ac6
Jessica Nunes <65510853+nunesjessica44@users.noreply.github.com>
2021-02-01 20:36:27 -0300

    criando componentes

---
+++ MinhaAppAngular/src/app/pasta/nome-componente/nome-componente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-component',
+  templateUrl: './nome-componente.component.html',
+  styleUrls: []
+})
+export class NomeComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
