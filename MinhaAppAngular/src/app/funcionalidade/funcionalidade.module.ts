faa77e61a6e4b350cfe761b4977a7bf00b1aaf04
Jessica Nunes <65510853+nunesjessica44@users.noreply.github.com>
2021-02-01 21:23:24 -0300

    criando modulos e serviços

---
+++ MinhaAppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    //Seus Componentes
+  ],
+  imports: [
+    CommonModule
+    //Seus Modulos
+  ],
+  providers: [
+    //Seus Serviços
+
+  ]
+})
+export class FuncionalidadeModule { }
